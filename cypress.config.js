2ac4eb6f4a3598fcf102a86ae08f5d049cc4489b
marisannag-debug <marisannag@gmail.com>
2026-03-26 01:29:28 +0100

    chore(cypress): add config and npm scripts; fix home spec

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    specPattern: 'cypress/integration/**/*.ts',
+    supportFile: false,
+    video: false,
+    screenshotOnRunFailure: false,
+  },
+}
--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,6 @@
 describe('Home', () => {
   it('shows CTA', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:3000')
     cy.contains('Załóż darmowy klub').should('be.visible')
   })
 })
