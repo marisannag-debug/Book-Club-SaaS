361a12a2ecc4f0183af94811eebe03a74321e794
marisannag-debug <marisannag@gmail.com>
2026-03-26 15:37:25 +0100

    test(e2e): intercept POST /api/clubs and assert 201 before redirect

--- cypress/integration/create-club.spec.ts
+++ cypress/integration/create-club.spec.ts
@@ -1,9 +1,13 @@
 describe('Create Club', () => {
   it('creates a club and redirects', () => {
+    cy.intercept('POST', '/api/clubs').as('createClub')
     cy.visit('/app/clubs/new')
     cy.get('[data-cy=create-club-name]').type('Cypress Club')
     cy.get('[data-cy=create-club-description]').type('E2E test club')
     cy.get('[data-cy=create-club-submit]').click()
+    cy.wait('@createClub', { timeout: 10000 }).then((inter) => {
+      expect(inter.response && inter.response.statusCode).to.equal(201)
+    })
     // Wait for redirect to /app/clubs/:id
     cy.location('pathname', { timeout: 10000 }).should((pathname) => {
       expect(pathname).to.match(/\/app\/clubs\/[-a-f0-9]{36}$/)
