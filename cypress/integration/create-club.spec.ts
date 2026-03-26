00f589de3a84df2722e591586f398baf25f03da3
marisannag-debug <marisannag@gmail.com>
2026-03-26 15:39:24 +0100

    test(e2e): robust typing with focus+force to avoid headless keyboard issues

--- cypress/integration/create-club.spec.ts
+++ cypress/integration/create-club.spec.ts
@@ -2,8 +2,8 @@ describe('Create Club', () => {
   it('creates a club and redirects', () => {
     cy.intercept('POST', '/api/clubs').as('createClub')
     cy.visit('/app/clubs/new')
-    cy.get('[data-cy=create-club-name]').type('Cypress Club')
-    cy.get('[data-cy=create-club-description]').type('E2E test club')
+    cy.get('[data-cy=create-club-name]').focus().type('Cypress Club', { force: true })
+    cy.get('[data-cy=create-club-description]').focus().type('E2E test club', { force: true })
     cy.get('[data-cy=create-club-submit]').click()
     cy.wait('@createClub', { timeout: 10000 }).then((inter) => {
       expect(inter.response && inter.response.statusCode).to.equal(201)
